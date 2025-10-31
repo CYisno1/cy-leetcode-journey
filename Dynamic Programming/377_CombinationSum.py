d431821a0a67744851846772d00a86df12fe2b40
CYisno1 <candice890301@gmail.com>
2025-10-31 13:37:05 -0400

    Create 377_CombinationSum.py

---
+++ Dynamic Programming/377_CombinationSum.py
@@ -0,0 +1,19 @@
+# Top-Down
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        
+        @functools.lru_cache(maxsize = None)
+        def combs(remain):
+            if remain == 0:
+                return 1
+            
+            result = 0
+            for num in nums:
+                if remain - num >= 0:
+                    result += combs(remain - num)
+                else:
+                    break
+            return result
+
+        return combs(target)
